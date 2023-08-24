6fc7afcc395289193f13643dce8c887b3bf35175
hiba_neh <107880774+hibounashi@users.noreply.github.com>
2023-08-24 18:17:07 +0100

    modified:   maic.js 	modified:   main.css 	modified:   main.html 	new file:   tempCodeRunnerFile.js

--- maic.js
+++ maic.js
@@ -1,1 +1,13 @@
-print
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerLink = document.querySelector('.burger');
+    const toggle = document.querySelector('.toggle');
+    const closeImg = document.querySelector('.xbtn');
+
+    burgerLink.addEventListener('click', () => {
+        toggle.classList.toggle('toggle-active');
+    });
+
+    closeImg.addEventListener('click', () => {
+        toggle.classList.remove('toggle-active');
+    });
+});
